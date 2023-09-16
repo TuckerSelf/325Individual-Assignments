bdd2d81c2b88542bc1e959471bd5cc404cec75bd
TuckerSelf <tself3@ut.utm.edu>
2023-09-16 12:20:04 -0500

    skeletal definitions

--- HW1/Vector.cpp
+++ HW1/Vector.cpp
@@ -9,5 +9,57 @@
 
 #include <iostream>
 #include <fstream>
+#include "Vector.h"
 
 using namespace std;
+
+Vector::Vector(){
+  vec_ptr = NULL;
+  vec_size = 0;
+  vec_capacity = 0;
+}
+
+Vector::Vector(const Vector &other){
+  vec_ptr = other.vec_ptr;
+  vec_size = other.vec_size;
+  vec_capacity = other.vec_capacity;
+}
+
+Vector::~Vector(){
+  delete[] vec_ptr;
+  vec_size = 0;
+  vec_capacity = 0;
+}
+
+Vector& Vector::operator=(const Vector &other){
+  vec_ptr = other.vec_ptr;
+  vec_size = other.vec_size;
+  vec_capacity = other.vec_capacity;
+  return *this;
+}
+
+int Vector::size(){
+  return vec_size;
+}
+
+int Vector::capacity(){
+  return vec_capacity;
+}
+
+void Vector::push_back(int element){
+  if(vec_ptr[size-1] == nullptr)
+    vec_ptr[size-1] = element;
+  else{
+    vec_ptr = new int (2*vec_capacity);
+    vec_ptr[size-1] = element;
+  }
+}
+
+void Vector::reserve(int n){
+  if(n > vec_capacity)
+    vec_ptr = new int [n];
+}
+
+int& Vector::operator[](unsigned int index){
+  return vec_ptr[index];
+}
